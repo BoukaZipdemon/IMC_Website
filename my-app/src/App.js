ba94a7f9baa38b0a5bb7d2c797483c5d2c17eafb
BoukaZipDemon <boukarifedi20@gmail.com>
2024-04-09 18:50:25 +0300

    this is the main structure

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>THE PROJECT IS UNDER CONSTRUCTION</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
