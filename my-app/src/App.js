ced1ec507e136c587354b0cbbec147ca26687e82
marambenamna <benamnamaram@gmail.com>
2024-04-13 21:31:18 +0100

    rany hatyt background fl home w ktebt welcome

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './Navbar'
+import Home from './Home';
 function App() {
   return (
     <div className="App">
       <div className="Content">
         <Navbar />
         <div className="home">
-              
+            <Home />    
         </div>
       </div>
     </div>
---
+++ my-app/src/Home.js
@@ -0,0 +1,13 @@
+const  Home = () => {
+
+
+    return ( 
+        <div className="home">
+            <h1>welcome to imc </h1>
+
+
+        </div>
+     );
+}
+ 
+export default (Home);
